44d0a6c7e76eaabcc8f9d47b5e60d463172c77e3
michaelreiff <herr.reiff@web.de>
2024-02-16 23:34:44 +0100

    bugs fixed

--- pxl.js
+++ pxl.js
@@ -1,23 +1,21 @@
-/*
-**  Definiere pxl als jQuery Plugin
-*/
 (function($) {
     // Definiere deine Klasse
-    var pxl = function(element, options) {
+    var PxlClass = function(element, options) {
         this.element = element;
         this.options = options;
     };
 
     // Erweitere deine Klasse um Methoden
-    pxl.prototype = {
-        
-        /*
-        *   Erstellt die Matrix
-        */
+    PxlClass.prototype = {
         create_matrix: function() {
-            alert("test");
+            // Implementiere die Logik für die Methode "create_matrix"
+            //alert("Create Matrix called");
+
+            // innerhalb des pxl's
             let x = 3;
             let y = 3;
+            
+            
             let zx = 3;
             let zy = 3;
             //console.log('1');
@@ -32,11 +30,14 @@
                     for (let k = 1; k < zx+1; k++) {
                         for (let l = 1; l < zy+1; l++) {
                             //alert(i);
-                            $(this.element).append("<div class='square k_"+k+"_"+i+"'>"+i+":"+j+":"+k+":"+l+"</div>");
+                            $(this.element).append("<div class='square pixel_"+k+"_"+i+" part_"+l+"_"+j+" k_"+k+"_"+i+"'>"+i+":"+j+":"+k+":"+l+"</div>");
                         }
                     }
                 }
             }
+            
+            
+            $("#create_matrix").prop("disabled", true);
             
             return this; // Rückgabe des Objekts für Methodenverkettung
         },
@@ -46,7 +47,10 @@
         **
         */
         create_pixel: function() {
-            
+            // Implementiere die Logik für die Methode "create_pixel"
+            alert("Create Pixel called");
+
+            /*            
             let x = 3;
             let y = 3;
             let zx = 3;
@@ -64,14 +68,24 @@
                 }
             }
             
+            */
+
+
+
             return this; // Rückgabe des Objekts für Methodenverkettung
         },
 
 
+        /*
+        **
+        */
         fill: function(data) {
             // Implementiere die Logik für die Methode "fill"
-            alert('fill-Methode aufgerufen mit Daten:');
-            //console.log('fill-Methode aufgerufen mit Daten:', data);
+            //alert('Fill Method called with data: ' + data);
+
+            $(".k_"+getRandomInt(1, 3)+"_"+getRandomInt(1, 3)).css("background-color", getRandomHexColor());
+            
+
             return this; // Rückgabe des Objekts für Methodenverkettung
         }
     };
@@ -81,30 +95,53 @@
         return this.each(function() {
             var element = $(this);
             // Prüfe, ob die Klasse bereits initialisiert wurde
-            var pxl = element.data('pxl');
-            if (!pxl) {
+            var pxlInstance = element.data('pxl');
+            if (!pxlInstance) {
                 // Initialisiere die Klasse und speichere sie im Datenattribut des Elements
-                instance = new pxl(element, options);
-                pxl.data('pxl', instance);
+                pxlInstance = new PxlClass(element, options);
+                element.data('pxl', pxlInstance);
             }
-            
-            // Überprüfe, ob das Element mit der ID "control" existiert
-            var controlElement = $('#control');
-            if (controlElement.length > 0) {
-                // Erstelle die Buttons "fill" und "create_matrix"
-                var fillButton = $('<button>Fill</button>').appendTo(controlElement);
-                var createButton = $('<button>Create</button>').appendTo(controlElement);
 
+            // Überprüfe, ob das Element mit der ID "control" existiert
+            var controlWrapper = $('#control');
+            if (controlWrapper.length > 0) {
+                // Erstelle die Buttons "fill" und "create_matrix"
+                var createButton = $('<button id="create_matrix">Create Matrix</button><br>').appendTo(controlWrapper);
+                var fillButton = $('<button id="fill">Fill random Pxl</button><br>').appendTo(controlWrapper);
+                
                 // Weise den Buttons die entsprechenden Methoden zu
                 fillButton.on('click', function() {
-                    pxl.fill();
+                    pxlInstance.fill('Some data');
                 });
 
                 createButton.on('click', function() {
-                    pxl.create_matrix();
-                    console.log(pxl);
+                    pxlInstance.create_matrix();
+
                 });
             }
         });
     };
 })(jQuery);
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Funktion, um eine zufällige Hex-Farbe zu generieren
+function getRandomHexColor() {
+    // Zufällige Werte für Rot, Grün und Blau generieren
+    var red = Math.floor(Math.random() * 256); // Wert zwischen 0 und 255
+    var green = Math.floor(Math.random() * 256); // Wert zwischen 0 und 255
+    var blue = Math.floor(Math.random() * 256); // Wert zwischen 0 und 255
+
+    // Hexadezimalwert erstellen
+    var hexColor = '#' + 
+        ('0' + red.toString(16)).slice(-2) + // Rot
+        ('0' + green.toString(16)).slice(-2) + // Grün
+        ('0' + blue.toString(16)).slice(-2); // Blau
+
+    return hexColor;
+}
+
+
+$("#box").pxl();
